7c60e6a00ee42666328b26c98a17494318dd3c46
ccol <chris.charalambous@otherlevels.com>
2021-10-26 19:53:40 +1100

    added test SSJS file for testing SSJS file in cloudpage

---
+++ anz-pp/ssjs/test-ssjs.js
@@ -0,0 +1,36 @@
+<!-- ENDED UP MOVING TO GITHUB PAGES-->
+
+<script runat=server>
+Platform.Load('core', '1');
+
+var fieldArr = [];
+
+for(var i = 0; i < srcFields.length; i++){
+    var field = {};
+    field.Name = srcFields[i].Name;
+    field.FieldType = srcFields[i].FieldType;
+    if(srcFields[i].FieldType == 'Text'){
+        field.MaxLength = srcFields[i].MaxLength;
+    }
+    fieldArr.push(field);
+}
+
+var guid = Platform.Function.GUID();
+var deName = srcDeNameKey + '_' + guid;
+
+var obj = {
+    CustomerKey: guid,
+    Name: deName,
+    Fields: fieldArr,
+    CategoryID: 68808
+};
+
+DataExtension.Add(obj);
+
+var data = srcDe.Rows.Retrieve();
+
+Write(Stringify(data));
+
+var targetDE = DataExtension.Init(guid);
+var count = targetDE.Rows(Add(data);
+</script>
